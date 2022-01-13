98886e256030ab630aa3562f5ec12628011d4f6a
franpvp <franpvp.98@gmail.com>
2022-01-13 02:35:07 -0300

    se agrega barra de colores

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,41 @@
-/*!
-* Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+obtenerAncho();
+window.addEventListener('resize', obtenerAncho);
+
+function obtenerAncho(){
+  var width = document.documentElement.clientWidth;
+  console.log(width);
+  generarBordes(width);  
+}
+
+
+var color = true;
+
+function generarColor(borde) {
+  if (color) {
+    borde.style.backgroundColor = "#FFA0B6";
+    color = false;
+  } else {
+    borde.style.backgroundColor = "#82E8D3";
+    color = true;
+  }
+}
+
+function generarBordes(width) {
+  let bordes = document.getElementById("bordes");
+  color=true;
+  bordes.innerHTML ="";
+  var cantidad = Math.trunc(width/30);
+
+  for (let index = 0; index < cantidad+1; index++) {
+    let borde = document.createElement("div");
+    borde.setAttribute("class", "borde");
+    generarColor(borde);
+    bordes.append(borde);
+    console.log("counter: " + index);
+  }
+}
+
+
+
+
+
